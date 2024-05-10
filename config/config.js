493d027c4bce50d54f8f4edae4120034a62a41fd
Sayed Khaidir Ali <sayed-k-ali@github.com>
2024-05-10 21:25:44 +0700

    fix config db

--- config/config.js
+++ config/config.js
@@ -17,4 +17,20 @@ module.exports = {
     }
 
   },
+  "development": {
+    "username": "binar-psql-db",
+    "password": "4GZfP_X1DS00SiOAhRl4Yw",
+    "database": "bingle-shop",
+    "host": "binar-psql-db-6765.6xw.aws-ap-southeast-1.cockroachlabs.cloud",
+    port: "26257",
+    "dialect": "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(__dirname + '/root.crt').toString(),
+      }
+    }
+
+  },
 }
