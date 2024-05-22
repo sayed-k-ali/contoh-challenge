8affea97738d008b6455b3398af6f2c744ee926a
Sayed Khaidir Ali <sayed-k-ali@github.com>
2024-05-22 22:05:23 +0700

    fix db config

--- config/config.js
+++ config/config.js
@@ -18,24 +18,23 @@ module.exports = {
     }
 
   },
-  // "development": {
-  //   "username": "binar-psql-db",
-  //   "password": "4GZfP_X1DS00SiOAhRl4Yw",
-  //   "database": "bingle-shop",
-  //   "host": "binar-psql-db-6765.6xw.aws-ap-southeast-1.cockroachlabs.cloud",
-  //   port: "26257",
-  //   "dialect": "postgres",
-  //   dialectModule: require('pg'),
-  //   dialectOptions: {
-  //     ssl: {
-  //       require: true,
-  //       rejectUnauthorized: true,
-  //       ca: fs.readFileSync(__dirname + '/root.crt').toString(),
-  //     }
-  //   }
-
-  // },
   "development": {
+    "username": "binar-psql-db",
+    "password": "4GZfP_X1DS00SiOAhRl4Yw",
+    "database": "bingle-shop",
+    "host": "binar-psql-db-6765.6xw.aws-ap-southeast-1.cockroachlabs.cloud",
+    port: "26257",
+    "dialect": "postgres",
+    dialectModule: require('pg'),
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(__dirname + '/root.crt').toString(),
+      }
+    }
+  },
+  "test": {
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DB,
